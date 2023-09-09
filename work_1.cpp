cb8794acd4703a0dcba52b1bc19e45cdb35b4596
Virgínia <virginiasoaresmuller@gmail.com>
2023-09-09 18:45:09 -0300

    trab

--- work_1.cpp
+++ work_1.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <filesystem>
+#include <chrono>
 
 using namespace std;
+namespace fs = std::filesystem;
+namespace chrono = std::chrono;
 
 // Function to perform mutations on DNA sequence
 string performMutations(const string& dna) {
@@ -23,22 +30,50 @@ string performMutations(const string& dna) {
 }
 
 int main() {
-    string DNA;
-    cout << "Enter the original DNA sequence: ";
-    cin >> DNA;
+    // Directory containing test files
+    string directoryPath = "casosTeste_t10";
 
-    string mutatedDNA = DNA;
+    // Iterate over files in the directory
+    for (const auto& entry : fs::directory_iterator(directoryPath)) {
+        if (entry.is_regular_file()) {
+            string fileName = entry.path().filename().string();
+            
+            // Check if the file has a .txt extension
+            if (fileName.substr(fileName.size() - 4) == ".txt") {
+                ifstream file(entry.path());
+                if (file.is_open()) {
+                    string DNA;
+                    // Read DNA sequence from the file
+                    getline(file, DNA);
+                    file.close();
 
-    while (true) {
-        string newDNA = performMutations(mutatedDNA);
-        if (newDNA == mutatedDNA) {
-            break;  // No further mutations possible
+                    string mutatedDNA = DNA;
+
+                    // Measure the processing time
+                    auto startTime = chrono::high_resolution_clock::now();
+
+                    while (true) {
+                        string newDNA = performMutations(mutatedDNA);
+                        if (newDNA == mutatedDNA) {
+                            break;  // No further mutations possible
+                        }
+                        mutatedDNA = newDNA;
+                    }
+
+                    auto endTime = chrono::high_resolution_clock::now();
+                    auto duration = chrono::duration_cast<chrono::milliseconds>(endTime - startTime);
+
+                    cout << "File: " << fileName << endl;
+                    cout << "Original DNA sequence: " << DNA << endl;
+                    cout << "Final DNA sequence: " << mutatedDNA << endl;
+                    cout << "Final DNA sequence length: " << mutatedDNA.size() << endl;
+                    cout << "Processing time: " << duration.count() << " milliseconds" << endl;
+                } else {
+                    cerr << "Error opening file: " << fileName << endl;
+                }
+            }
         }
-        mutatedDNA = newDNA;
     }
-
-    cout << "Final DNA sequence: " << mutatedDNA << endl;
-    cout << "Final DNA sequence length: " << mutatedDNA.size() << endl;
 
     return 0;
 }
